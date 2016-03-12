bf9a5e72c8d2bc7376de35ea59423b05db3b67db
Nikema Prophet <nikema@gmail.com>
2016-03-12 02:52:09 -0800

    Added dragging of images

--- mylittlemonster/ViewController.swift
+++ mylittlemonster/ViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
     @IBOutlet weak var monsterImg: UIImageView!
-    
+    @IBOutlet weak var foodImg: DragImg!
+    @IBOutlet weak var heartImg: DragImg!
     
     override func viewDidLoad() {
         super.viewDidLoad()
